bf024f6ff64ed15bbc73968dd80f153b5a582390
rain <GameEngine@163.com>
2020-06-29 16:29:07 +0800

    gd code style

--- assets/scripts/chessboard.gd
+++ assets/scripts/chessboard.gd
@@ -13,7 +13,7 @@ export(int) var confirm_max=3
 var confirm_count:int=0
 
 var chessScene=preload('res://assets/scenes/chess.tscn')
-onready var btn_restart:Button=$CenterContainer/Button
+onready var btn_restart:Button=$VBoxContainer/Button
 onready var chessTip:Sprite=$chessTip
 onready var downChessSample:AudioStreamPlayer=$chessSample
 
@@ -31,36 +31,36 @@ func init()->void:
 	CHESS_ARRAY.resize(15*15)
 	chessTip.texture=Global.gb_texture
 
-func setChess(x,y:int,s:Chess)->void:
-	assert(inChessboard(x,y))
+func set_chess(x,y:int,s:Chess)->void:
+	assert(in_chessboard(x,y))
 	CHESS_ARRAY[x+y*CHESS_NUMBER]=s
 
-func inChessboard(x,y:int)->bool:
+func in_chessboard(x,y:int)->bool:
 	return ! (x<0 || x>CHESS_NUMBER-1 || y<0 || y>CHESS_NUMBER-1)
 
 
-func getChess(x,y:int)->Chess:
-	if !inChessboard(x,y):
+func get_chess(x,y:int)->Chess:
+	if !in_chessboard(x,y):
 		return null
 	
 	return CHESS_ARRAY[x+y*CHESS_NUMBER] as Chess
 
-func getChessState(x,y:int)->int:
-	var sprite=getChess(x,y)
+func get_chessState(x,y:int)->int:
+	var sprite=get_chess(x,y)
 	if sprite!=null:
 		return (sprite as Chess).ChessColor
 		
 	return Global.CHESS_COLOR.CC_INVALID
 		
-func checkChessFull()->bool:
+func check_chessFull()->bool:
 	return CHESS_ARRAY.count(null)==0
 		
 #检查是否五子连通
 #p 最后下的位置
-func checkChessSuccess(p:Vector2)->bool:
+func check_chessSuccess(p:Vector2)->bool:
 	var x:int=int(p.x)
 	var y:int=int(p.y)
-	var c=getChessState(x,y)
+	var c=get_chessState(x,y)
 	
 	var ha=[p]
 	var va=[p]
@@ -69,52 +69,52 @@ func checkChessSuccess(p:Vector2)->bool:
 	
 	#横
 	for i in range(1,5):
-		if getChessState(i+x,y)!=c:
+		if get_chessState(i+x,y)!=c:
 			break
 		else:
 			ha.append(Vector2(i+x,y))
 			
 	for i in range(-1,-5,-1):
-		if getChessState(i+x,y)!=c:
+		if get_chessState(i+x,y)!=c:
 			break
 		else:
 			ha.append(Vector2(i+x,y))
 			
 	#竖
 	for i in range(1,5):
-		if getChessState(x,i+y)!=c:
+		if get_chessState(x,i+y)!=c:
 			break
 		else:
 			va.append(Vector2(x,i+y))
 			
 	for i in range(-1,-5,-1):
-		if getChessState(x,i+y)!=c:
+		if get_chessState(x,i+y)!=c:
 			break
 		else:
 			va.append(Vector2(x,i+y))
 			
 	#斜下
 	for i in range(1,5):
-		if getChessState(x+i,y+i)!=c:
+		if get_chessState(x+i,y+i)!=c:
 			break
 		else:
 			sa_d.append(Vector2(x+i,y+i))
 			
 	for i in range(-1,-5,-1):
-		if getChessState(x+i,y+i)!=c:
+		if get_chessState(x+i,y+i)!=c:
 			break
 		else:
 			sa_d.append(Vector2(x+i,y+i))
 			
 	#斜上
 	for i in range(1,5):
-		if getChessState(x+i,y-i)!=c:
+		if get_chessState(x+i,y-i)!=c:
 			break
 		else:
 			sa_u.append(Vector2(x+i,y-i))
 			
 	for i in range(-1,-5,-1):
-		if getChessState(x+i,y-i)!=c:
+		if get_chessState(x+i,y-i)!=c:
 			break
 		else:
 			sa_u.append(Vector2(x+i,y-i))
@@ -132,34 +132,34 @@ func checkChessSuccess(p:Vector2)->bool:
 		
 		if ha.size()>4:
 			for p in ha:
-				getChess(p.x,p.y).flash()
+				get_chess(p.x,p.y).flash()
 				
 		if va.size()>4:
 			for p in va:
-				getChess(p.x,p.y).flash()
+				get_chess(p.x,p.y).flash()
 				
 		if sa_d.size()>4:
 			for p in sa_d:
-				getChess(p.x,p.y).flash()
+				get_chess(p.x,p.y).flash()
 				
 				
 		if sa_u.size()>4:
 			for p in sa_u:
-				getChess(p.x,p.y).flash()
+				get_chess(p.x,p.y).flash()
 		return true
 	
-	return checkChessFull()
+	return check_chessFull()
 
 
 #v 逻辑坐标
-func createChess(v:Vector2,cc:int)->Sprite:
+func create_chess(v:Vector2,cc:int)->Sprite:
 	var x:int=int(round(v.x))
 	var y:int=int(round(v.y))
 	
-	assert(inChessboard(x,y))
+	assert(in_chessboard(x,y))
 
 
-	if getChess(x,y)!=null:
+	if get_chess(x,y)!=null:
 		return null
 		
 	downChessSample.play()
@@ -170,35 +170,35 @@ func createChess(v:Vector2,cc:int)->Sprite:
 	add_child(sprite)
 	return sprite
 
-func confirmChess(chess:Chess)->void:
+func confirm_chess(chess:Chess)->void:
 	IS_CONFIRM=true
 	chess.confirm()
 	yield(chess,'confirm_signal')
 	IS_CONFIRM=false
 
-func putChess(e:InputEventMouseButton):
+func put_chess(e:InputEventMouseButton):
 	var v=(e.position - CHESS_ORIGIN) / CHESS_SPACE
 	var vx:int=int(round(v.x))
 	var vy:int=int(round(v.y))
 
-	if inChessboard(vx,vy):
-		if IS_CONFIRM && lastChess!=null && getChess(vx,vy)==null:
+	if in_chessboard(vx,vy):
+		if IS_CONFIRM && lastChess!=null && get_chess(vx,vy)==null:
 			var lastP=((lastChess.position - CHESS_ORIGIN) / CHESS_SPACE ).round()
-			setChess(int(lastP.x),int(lastP.y),null)
+			set_chess(int(lastP.x),int(lastP.y),null)
 			lastChess.queue_free()
 			lastChess=null
 			confirm_count+=1
 	
-		var chess= createChess(v,current_input)
+		var chess= create_chess(v,current_input)
 		if chess!=null:
-			setChess(vx,vy,chess) #先放置,用于判断是否重复确认
+			set_chess(vx,vy,chess) #先放置,用于判断是否重复确认
 			if confirm_count<confirm_max:
-				yield(confirmChess(chess),'completed')
+				yield(confirm_chess(chess),'completed')
 			else:
 				IS_CONFIRM=false
 				
 			confirm_count=0
-			if !checkChessSuccess(v.round()):
+			if !check_chessSuccess(v.round()):
 				if current_input==Global.CHESS_COLOR.CC_BLACK:
 					current_input=Global.CHESS_COLOR.CC_WHITE
 					chessTip.texture=Global.gw_texture
@@ -211,16 +211,35 @@ func putChess(e:InputEventMouseButton):
 				btn_restart.visible=true
 
 
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		var e=event as InputEventMouseButton
 		if e.pressed && !IS_WIN:
-			putChess(e)
+			put_chess(e)
+
+func query_wanIP()->void:
+	if $HTTPRequest.connect("request_completed", self, "_on_request_completed") == OK:
+		$HTTPRequest.request('https://www.charfun.com/api/ipapi')
+	else:
+		print("connect is error!")
 
 func _ready() -> void:
 	btn_restart.visible=false
 	init()
 
+
 func _on_Button_pressed() -> void:
 	btn_restart.visible=false
 	init()
+	
+func _on_request_completed(_result, response_code, _headers, body):
+	if response_code==200:
+		var json = JSON.parse(body.get_string_from_utf8())
+		$VBoxContainer/Label.text=json.result['data']['ip']
+	else:
+		print(response_code)
+
+
+func _on_Button2_pressed() -> void:
+	query_wanIP()
